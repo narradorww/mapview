c9571a830edd45b3e4bfe426289a071cbfb2ef56
narradorww <rodrigo.anst@gmail.com>
2021-11-08 13:45:57 -0300

    Created the layout, installed @types dependecy of styled-components and created the folder 'components'

---
+++ mapview/src/container/Home/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+//import Map from '../../components/Map/index'
+import {Container, Aside, StyledApp, Header} from './style'
+
+export default function Home(){
+    return (
+        <>
+        <Header>Logo NetLume</Header>
+        <Container>
+        <Aside>
+            <div> Control Panel </div>
+        </Aside>
+        <StyledApp>
+            <div> Map </div>
+            
+            <p>Display</p>
+            </StyledApp>
+        
+        </Container>
+        </>
+
+
+    )
+};
---
+++ mapview/src/container/Home/style.ts
@@ -0,0 +1,30 @@
+
+import styled from 'styled-components';
+
+
+export const Header = styled.header`
+    height: 80px;
+    background-color: red;
+    color: white;
+    padding: 20px;
+`;
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: row;
+`;
+
+export const Aside = styled.aside`
+    background-color: black;
+    color: white;
+    width: 20%;
+    height: 100vh;
+`;
+
+export const StyledApp = styled.div`
+    display: flex;
+    flex-direction: column;
+    background-color: purple;
+    height: 120 px;
+    
+`;
