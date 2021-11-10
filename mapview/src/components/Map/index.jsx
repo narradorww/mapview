80edffaec6497d8c2faf5be86e0f12e3c5445aa9
narradorww <rodrigo.anst@gmail.com>
2021-11-10 00:15:08 -0300

    map consuming the endpoint's GeoJSON

--- mapview/src/components/Map/index.jsx
+++ mapview/src/components/Map/index.jsx
@@ -4,25 +4,23 @@ import "../Map/style.css";
 import "leaflet/dist/leaflet.css";
 import osm from "../../constants/osm-provider";
 import { useGeoJSON } from "../../hooks/useGeoJSON";
-import geo from "../../constants/geoJSON.json";
 
 
 export default function Map() {
   const [center, setCenter] = useState({ lat: 12.041833166, lng: 8.521331248 });
-  const { getAll, geoJson } = useGeoJSON();
-
-  console.log({ ...geoJson});
+  const { getAll, geoJSON } = useGeoJSON();
+  console.log({ ...geoJSON });
 
   useEffect(() => {
     getAll();
-  },[getAll])
-
-  
+  }, [getAll]);
 
   return (
-    <MapContainer id="map" center={center} zoom={10} scrollWheelZoom={true}>
-      <TileLayer url={osm.url} attribution={osm.attribution} />
-      <GeoJSON data={geo} />
-    </MapContainer>
+    <>
+      <MapContainer id="map" center={center} zoom={10} scrollWheelZoom={true}>
+        <TileLayer url={osm.url} attribution={osm.attribution} />
+        <GeoJSON data = {geoJSON} />
+      </MapContainer>
+    </>
   );
 }
