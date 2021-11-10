9d886fd400db7f50512650a3658de6a77c20eb6c
narradorww <rodrigo.anst@gmail.com>
2021-11-10 10:34:57 -0300

    Add eventHandler in GeoJSON Layer

--- mapview/src/components/Map/index.jsx
+++ mapview/src/components/Map/index.jsx
@@ -4,7 +4,7 @@ import "../Map/style.css";
 import "leaflet/dist/leaflet.css";
 import osm from "../../constants/osm-provider";
 import { useGeoJSON } from "../../hooks/useGeoJSON";
-
+import geo from "../../constants/geoJSON.json";
 
 export default function Map() {
   const [center, setCenter] = useState({ lat: 12.041833166, lng: 8.521331248 });
@@ -15,12 +15,29 @@ export default function Map() {
     getAll();
   }, [getAll]);
 
+  function onEachFeature(feature, layer) {
+    if (feature.properties && feature.properties.name) {
+        layer.bindPopup(feature.properties.name);
+        console.log(`ja sabemos como funfa ${feature.properties.uuid}`)
+    }
+}
+
+
+
+
+
+
   return (
     <>
       <MapContainer id="map" center={center} zoom={10} scrollWheelZoom={true}>
         <TileLayer url={osm.url} attribution={osm.attribution} />
-        <GeoJSON data = {geoJSON} />
+        <GeoJSON data={geo} onEachFeature={onEachFeature} eventHandlers={{
+    click: () => {
+      console.log('marker clicked')
+    },
+  }}/>
       </MapContainer>
     </>
   );
 }
+
